26cd3b5782671d33e6c9f6110c71628f42d7690a
Tamer Ahmed <AhmedT@aecl.com>
2018-10-08 10:11:41 +0300

    Add project files.

---
+++ IoT POC/IoT POC/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using IoT_POC.Models;
+
+namespace IoT_POC.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
